d8086fd2b7035c46a51e81fa0feae4eb1ae6d8f4
donghwan <donghwan@twinny.co.kr>
2022-12-21 12:36:59 +0900

    CacheLock 통합 테스트 추가

--- tests/tests/cache_lock/test_cache_lock.py
+++ tests/tests/cache_lock/test_cache_lock.py
@@ -1,10 +1,66 @@
+import functools
 from uuid import uuid4
+from multiprocessing import Pool
 
 from django.test import SimpleTestCase
 from django.core.cache import cache
 
-from django_cache_lock import CacheLock
+from django_cache_lock import CacheLock, mutex
 from django_cache_lock.settings import settings
+
+
+class CacheLockIntegrationtest(SimpleTestCase):
+    @staticmethod
+    def increase_cache_value(key: str):
+        value = cache.get(key, 0)
+        value += 1
+        cache.set(key, value)
+
+    @classmethod
+    def increase_cache_value_with_lock(cls, cache_lock_key: str, date_key: str):
+        lock = CacheLock(cache_lock_key)
+        lock.acquire()
+        cls.increase_cache_value(date_key)
+        lock.release()
+
+    def test_cache_lock(self):
+        cache_lock_key = str(uuid4())
+        date_key = "test-data-key"
+        test_function = functools.partial(CacheLockIntegrationtest.increase_cache_value_with_lock, cache_lock_key)
+        with Pool(10) as p:
+            p.map(test_function, [date_key] * 1000)
+        self.assertEqual(cache.get(date_key, 0), 1000)
+        cache.delete(date_key)
+
+    @classmethod
+    def increase_cache_value_with_context_manager(cls, cache_lock_key: str, date_key: str):
+        with CacheLock(cache_lock_key):
+            cls.increase_cache_value(date_key)
+
+    def test_cache_lock_with_context_manager(self):
+        cache_lock_key = str(uuid4())
+        date_key = "test-data-key"
+        test_function = functools.partial(
+            CacheLockIntegrationtest.increase_cache_value_with_context_manager,
+            cache_lock_key,
+        )
+        with Pool(10) as p:
+            p.map(test_function, [date_key] * 1000)
+        self.assertEqual(cache.get(date_key, 0), 1000)
+        cache.delete(date_key)
+
+    @classmethod
+    @mutex(key=str(uuid4()))
+    def increase_cache_value_with_decorator(cls, date_key: str):
+        cls.increase_cache_value(date_key)
+
+    def test_cache_lock_with_decorator(self):
+        date_key = "test-data-key"
+        test_function = CacheLockIntegrationtest.increase_cache_value_with_decorator
+        with Pool(10) as p:
+            p.map(test_function, [date_key] * 1000)
+        self.assertEqual(cache.get(date_key, 0), 1000)
+        cache.delete(date_key)
 
 
 class CacheLockUnitTest(SimpleTestCase):
