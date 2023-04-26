b839253745029cbf178a34d2650764dec40d8f4a
donghwan <donghwan@twinny.co.kr>
2023-04-26 15:04:52 +0900

    테스트에서 django-redis를 사용하도록 변경

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -8,7 +8,10 @@ INSTALLED_APPS = ["django_cache_lock"]
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
     }
 }
